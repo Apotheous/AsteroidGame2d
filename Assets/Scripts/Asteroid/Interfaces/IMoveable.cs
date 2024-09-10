32389c3bbfcc3a76549a904ee25c66fd94616033
Apotheous <brucoglu@outlook.com>
2024-09-10 16:19:02 +0300

    add asteroid pack

---
+++ Assets/Scripts/Asteroid/Interfaces/IMoveable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMoveable 
+{
+    Rigidbody rb { get; set; }
+
+    bool IsMovingForward { get; set; }
+
+    void MoveEnemyTowardsTarget(GameObject enemyObject, float MoveSpeed);
+
+    void CheckForForwardOrBackFacing(Vector3 velocity);
+}
