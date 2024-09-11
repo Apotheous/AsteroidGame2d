aca688c37e978fce39b26b0268c2182fadaaf44a
Apotheous <brucoglu@outlook.com>
2024-09-11 12:32:47 +0300

    asteroid properties

--- Assets/Scripts/Asteroid/Base/Asteroid.cs
+++ Assets/Scripts/Asteroid/Base/Asteroid.cs
@@ -5,24 +5,27 @@ using UnityEngine.UI;
 
 public class Asteroid : MonoBehaviour, IDamageable
 {
+    public AsteroidType asteroidType;
+
     public float maxHealth;
     public float CurrentHealth;
+
+    //public float scale;
+    public float damage;
+
     public Image healthBar;
     private void Start()
     {
-        CurrentHealth = 100;
-        Debug.Log("Asteroid -+-+- CurrentHealth = " + CurrentHealth);
-        Debug.Log("Asteroid -+-+- MaxHealth = " + maxHealth);
+        SetAsteroidProperties(asteroidType);
+        CurrentHealth = maxHealth;
+        
     }
     public void IDamage(float damageAmount)
     {
-        Debug.Log("Asteroid -+-+- Damage = " + CurrentHealth);
-        Debug.Log("Asteroid -+-+- Damage = " + maxHealth);
         CurrentHealth -= damageAmount;
         healthBar.fillAmount = CurrentHealth / maxHealth;
         if (CurrentHealth <= 0)
         {
-            Debug.Log("Asteroid +-+- Damage = " + CurrentHealth);
             IDie();
         }
     }
@@ -30,7 +33,6 @@ public class Asteroid : MonoBehaviour, IDamageable
     public void IDie()
     {
         Debug.Log("Asteroid +-+- die");
-        //StateMachine.ChangeState(DieState);
         Destroy(this.gameObject);
     }
 
@@ -38,5 +40,33 @@ public class Asteroid : MonoBehaviour, IDamageable
     {
         IDamage(20);
     }
+
+    private void SetAsteroidProperties(AsteroidType asteroidType)
+    {
+        switch (asteroidType)
+        {
+            case AsteroidType.LEVEL_SMALL:
+                maxHealth = 50f;
+                damage = 10;
+                break;
+            case AsteroidType.LEVEL_MID:
+                maxHealth = 100f;
+                damage = 30;
+                break;
+            case AsteroidType.LEVEL_BIG:
+                maxHealth = 200f;
+                damage = 50;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum AsteroidType
+{
+    LEVEL_SMALL,
+    LEVEL_MID, 
+    LEVEL_BIG
 }
 
