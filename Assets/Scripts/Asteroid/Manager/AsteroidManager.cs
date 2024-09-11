d585779a8d638fee3ffce96d08f21a6219ea3f79
Apotheous <brucoglu@outlook.com>
2024-09-11 03:58:02 +0300

    asteroid manager

---
+++ Assets/Scripts/Asteroid/Manager/AsteroidManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AsteroidManager : MonoBehaviour, IMoveable
+{
+
+    public float moveSpeed;
+    //public float moveRightSpeed;
+
+    public List<Asteroid> myAsteroids = new List<Asteroid>();
+
+    public Transform mySpawnPoint;
+
+    private float spawnTimer;
+    public float spawnTimerLate;
+    private int currentSpawnAst;
+    private int currentSpawnPoint;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnAsteroid", 0f, 0.5f);
+    }
+
+    void Update()
+    {
+        IMoveable(moveSpeed);
+
+    }
+
+    private void SpawnAsteroid()
+    {
+
+        Asteroid ast = Instantiate(myAsteroids[currentSpawnAst], mySpawnPoint);
+        ast.transform.position = new Vector3(Random.Range(-8f, 8f), mySpawnPoint.position.y, 0);
+        ast.transform.SetParent(null);
+ 
+        currentSpawnAst++;
+        currentSpawnPoint++;
+        if (currentSpawnAst >= myAsteroids.Count)
+        {
+            currentSpawnAst = 0;
+        }
+
+    }
+
+    public void IMoveable(float MoveSpeed)
+    {
+        transform.Translate(transform.up * MoveSpeed * Time.deltaTime);
+    }
+}
